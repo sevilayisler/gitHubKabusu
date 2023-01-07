c59e13812590214bb3a46db304fad118ef43d781
sevilayisler <53sevilay531@gmail.com>
2023-01-08 02:40:32 +0300

    first commit

---
+++ src/OrtakKullanim.java
@@ -0,0 +1,5 @@
+public class OrtakKullanim {
+    public static void main(String[] args) {
+        System.out.println("Buradan bir Ayse gecti");
+    }
+}
---
+++ src/US01.java
@@ -0,0 +1,15 @@
+public class US01 {
+
+    public static void main(String[] args) {
+        System.out.println("Burasi benim coplulgum-Ayse");
+    }
+}
+/*
+echo "# gitHubKabusu" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/sevilayisler/gitHubKabusu.git
+git push -u origin main
+ */
